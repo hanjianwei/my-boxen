820fc92aa1863b791512dd94598b4be335a4d109
Jianwei Han <hanjianwei@gmail.com>
2014-07-18 07:52:57 +0800

    Install Homebrew in standard location

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,5 @@
 require boxen::environment
 require homebrew
-require gcc
 
 Exec {
   group       => 'staff',
@@ -53,10 +52,8 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  include dnsmasq
   include git
   include hub
-  include nginx
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
@@ -69,16 +66,8 @@ node default {
 
   # default ruby versions
   ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
   ruby::version { '2.1.2': }
   include ruby::global
-
-  # common, useful packages
-  package {
-    [
-      'ack'
-    ]:
-  }
 
   file { "${boxen::config::srcdir}/our-boxen":
     ensure => link,
